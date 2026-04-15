59797c89d5e0a4244f8a83331cdedbee2d9e6f6a
JHONCHITO <jhon.chito01@unicatolica.edu.co>
2026-04-15 01:41:06 -0500

    fix: remove useRef error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
